666faf09a641517c18e90012a5b724baa5227828
carlcs <seelbach@gmx.de>
2016-05-09 22:59:17 +0200

    Added support for private email addresses

--- providers/social/Github.php
+++ providers/social/Github.php
@@ -8,6 +8,7 @@
 namespace Dukt\Social\LoginProviders;
 
 use Craft\Craft;
+use Guzzle\Http\Client;
 use Craft\Oauth_TokenModel;
 
 class Github extends BaseProvider
@@ -22,24 +23,54 @@ class Github extends BaseProvider
         return 'github';
     }
 
+    /**
+     * @inheritDoc
+     */
+    public function getDefaultScope()
+    {
+        return [
+            'user:email'
+        ];
+    }
+
     public function getProfile(Oauth_TokenModel $token)
 	{
 		$remoteProfile = $this->getRemoteProfile($token);
 		$remoteProfileArray = $remoteProfile->toArray();
 
-		$photoUrl = null;
-
-		if(isset($remoteProfileArray['avatar_url']))
-		{
-			$photoUrl = $remoteProfileArray['avatar_url'];
-		}
+        $email = (isset($remoteProfileArray['email']) ? $remoteProfileArray['email'] : $this->getRemoteEmail($token));
+        $photoUrl = (isset($remoteProfileArray['avatar_url']) ? $remoteProfileArray['avatar_url'] : null);
 
 		return [
 			'id' => $remoteProfile->getId(),
-			'email' => $remoteProfile->getEmail(),
+			'email' => $email,
 			'photoUrl' => $photoUrl,
 
 			'name' => $remoteProfile->getName(),
 		];
 	}
+
+    private function getRemoteEmail($token)
+    {
+        $oauthProvider = $this->getOauthProvider();
+
+        $client = new Client('https://api.github.com');
+        $client->addSubscriber($oauthProvider->getSubscriber($token));
+
+        $request = $client->get('/user/emails');
+
+        $response = $request->send();
+        $emails = $response->json();
+
+        if (is_array($emails))
+        {
+            foreach ($emails as $email)
+            {
+                if (isset($email['primary']) && $email['primary'])
+                {
+                    return $email['email'];
+                }
+            }
+        }
+    }
 }
