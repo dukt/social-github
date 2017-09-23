5c8ccfd54e04303dd25b8fc72f9db06abb52b878
Benjamin David <ben@dukt.net>
2017-09-23 18:38:52 +0200

    Now using `dukt\social\events\RegisterLoginProviderTypesEvent` event to register the GitHub login provider

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,6 +1,9 @@
 <?php
 namespace dukt\social\github;
 
+use dukt\social\services\LoginProviders;
+use yii\base\Event;
+
 /**
  * Plugin represents the GitHub integration plugin.
  *
@@ -10,14 +13,18 @@ namespace dukt\social\github;
 class Plugin extends \craft\base\Plugin
 {
     /**
-     * Returns Social login provider class names.
-     *
-     * @return array
+     * @inheritdoc
      */
-    public function getSocialLoginProviders()
+    public function init()
     {
-        return [
-            'dukt\social\github\loginproviders\Github'
-        ];
+        parent::init();
+
+        Event::on(LoginProviders::class, LoginProviders::EVENT_REGISTER_LOGIN_PROVIDER_TYPES, function($event) {
+            $loginProviderTypes = [
+                'dukt\social\github\loginproviders\Github'
+            ];
+
+            $event->loginProviderTypes = array_merge($event->loginProviderTypes, $loginProviderTypes);
+        });
     }
 }
